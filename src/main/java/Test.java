ac81efbeddde96df449d8e3010959f3e61e1f930
Olga Borodina <olga_borodina@epam.com>
2020-05-02 22:16:11 +0300

    fix import

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,9 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Test {
     public static void main(String[] args) {
